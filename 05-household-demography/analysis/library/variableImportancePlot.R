5811dae30eb2c90272ae199823a354352983df58
Andreas Angourakis <andros.spica@gmail.com>
2026-02-22 15:15:50 +0100

    Refactor variableImportancePlot and add v1.1 analysis
    Remove the old householdDemography.Rmd and introduce a new, cleaned analysis notebook (v-1-1.Rmd) with its generated HTML (v-1-1.nb.html) for the v1.1 sensitivity analysis. Refactor library/variableImportancePlot.R: add a title_extra parameter to variableImportancePlot, update variableImportancePlotPair to accept title_extra for each panel, simplify layout handling, restore graphics parameters properly, and streamline plotting logic (removed custom axis-label overlay code). These changes improve plot annotations and make side-by-side importance plots reusable for the new analysis notebook.

--- 05-household-demography/analysis/library/variableImportancePlot.R
+++ 05-household-demography/analysis/library/variableImportancePlot.R
@@ -1,9 +1,7 @@
-variableImportancePlot <- function(random_forest_results, plot_scale = 1, color_variable_map = NULL, log_scale = FALSE, range_standardised = FALSE)
+variableImportancePlot <- function(random_forest_results, title_extra = "", plot_scale = 1, color_variable_map = NULL, log_scale = FALSE, range_standardised = FALSE)
 {
   variableImportance <- randomForest::importance(random_forest_results)
   variableImportance <- variableImportance[nrow(variableImportance):1, ] # invert row order for plotting top-downwards
-  
-  
   
   # scale to 0-1 range
   if (range_standardised) {
@@ -25,57 +23,76 @@ variableImportancePlot <- function(random_forest_results, plot_scale = 1, color_
   dotchart(variableImportance, 
            color = color_variable_map,
            cex = plot_scale,
-           main = paste("Dependent variable: ", random_forest_results$terms[[2]]),
+           main = paste0(title_extra, "\nDependent variable: ", random_forest_results$terms[[2]]),
            pch = 19,
            log = ifelse(log_scale, "x", ""),
            xlab = paste("indicator score", ifelse(range_standardised, " (standardised)", "")))
 }
 
-variableImportancePlotPair <- function(random_forest_results_1, 
-                                       random_forest_results_2, 
+variableImportancePlotPair <- function(random_forest_results_1,
+                                       random_forest_results_2,
+                                       title_extra = c("", ""),
                                        plot_scale = 1,
                                        color_variable_map = NULL,
-                                       log_scale = FALSE, 
+                                       log_scale = FALSE,
                                        range_standardised = FALSE,
-                                       layout_type = 'vertical',
-                                       x_axis_label_x_position = 0.8 # c(0.3, 0.83)
-                                       )
+                                       layout_type = 'vertical'#,
+                                       #x_axis_label_x_position = 0.8 # c(0.3, 0.83)
+)
 {
-  par(mar = c(3,1,3,1), cex.lab = plot_scale * 0.5)
-  
   if (layout_type == 'horizontal')
   {
-    layout(matrix(c(1,2,3,3), ncol = 2, byrow = T),
-           heights = c(12,1))
+    layout(matrix(1:2, ncol = 2))
   }
   if (layout_type == 'vertical')
   {
-    layout(matrix(c(1,2,3), nrow = 3, byrow = T),
-           heights = c(12,12,1))
+    layout(matrix(1:2, nrow = 2))
   }
   
-  variableImportancePlot(random_forest_results_1, plot_scale = plot_scale * 0.8, color_variable_map = color_variable_map, log = log[1])
+  op <- par(no.readonly = TRUE)
   
-  variableImportancePlot(random_forest_results_2, plot_scale = plot_scale * 0.8, color_variable_map = color_variable_map, log = log[2])
+  par(mar = c(5, 4, 4, 2) + 0.1)
+  variableImportancePlot(random_forest_results_1, title_extra = title_extra[1], plot_scale = plot_scale, color_variable_map = color_variable_map, range_standardised = range_standardised)
   
-  par(mar = c(0, 0, 0, 0))
+  par(mar = c(5, 4, 4, 2) + 0.1)
+  variableImportancePlot(random_forest_results_2, title_extra = title_extra[2], plot_scale = plot_scale, color_variable_map = color_variable_map, range_standardised = range_standardised)
   
-  plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-  
-  if (layout_type == 'horizontal')
-  {
-    text(x = x_axis_label_x_position[1], y = 0.8, 
-         expression(paste(log[10], x)), 
-         cex = 1.6 * plot_scale, col = "black", font = 4)
-    text(x = x_axis_label_x_position[2], y = 0.8, 
-         expression(paste(log[10], x)), 
-         cex = 1.6 * plot_scale, col = "black", font = 4)
-  }
-  if (layout_type == 'vertical')
-  {
-    text(x = x_axis_label_x_position, y = 0.8, 
-         expression(paste(log[10], x)), 
-         cex = 1.6 * plot_scale, col = "black", font = 4)
-  }
-  
+  par(op)
+#   par(mar = c(3,1,3,1), cex.lab = plot_scale * 0.5)
+#   
+#   if (layout_type == 'horizontal')
+#   {
+#     layout(matrix(c(1,2,3,3), ncol = 2, byrow = T),
+#            heights = c(12,1))
+#   }
+#   if (layout_type == 'vertical')
+#   {
+#     layout(matrix(c(1,2,3), nrow = 3, byrow = T),
+#            heights = c(12,12,1))
+#   }
+#   
+#   variableImportancePlot(random_forest_results_1, title_extra = title_extra[1], plot_scale = plot_scale * 0.8, color_variable_map = color_variable_map, log = log_scale)
+#   
+#   variableImportancePlot(random_forest_results_2, title_extra = title_extra[2], plot_scale = plot_scale * 0.8, color_variable_map = color_variable_map, log = log_scale)
+#   
+#   par(mar = c(0, 0, 0, 0))
+#   
+#   plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+#   
+#   if (layout_type == 'horizontal')
+#   {
+#     text(x = x_axis_label_x_position[1], y = 0.8, 
+#          expression(paste(log[10], x)), 
+#          cex = 1.6 * plot_scale, col = "black", font = 4)
+#     text(x = x_axis_label_x_position[2], y = 0.8, 
+#          expression(paste(log[10], x)), 
+#          cex = 1.6 * plot_scale, col = "black", font = 4)
+#   }
+#   if (layout_type == 'vertical')
+#   {
+#     text(x = x_axis_label_x_position, y = 0.8, 
+#          expression(paste(log[10], x)), 
+#          cex = 1.6 * plot_scale, col = "black", font = 4)
+#   }
+#   
 }
